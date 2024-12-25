a32ce4dcbc09e91db74d36e77b02604170652f69
ahmedgabr200 <159725619+ahmedgabr200@users.noreply.github.com>
2024-12-25 02:10:09 +0200

    first commit

---
+++ build/precache-manifest.c257667285286148604a10676c49db59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "555e169554b566e9ab90f90f773af09d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87127a58b759e60dc1ca",
+    "url": "/static/css/2.d7cfef25.chunk.css"
+  },
+  {
+    "revision": "a5e2ed5ac49faba7f01b",
+    "url": "/static/css/main.a1272fed.chunk.css"
+  },
+  {
+    "revision": "87127a58b759e60dc1ca",
+    "url": "/static/js/2.b8de7d9e.chunk.js"
+  },
+  {
+    "revision": "774152ec3dfb5dadc6880f2e7f79e400",
+    "url": "/static/js/2.b8de7d9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5e2ed5ac49faba7f01b",
+    "url": "/static/js/main.eb72353a.chunk.js"
+  },
+  {
+    "revision": "40ba6fc54daca019862a",
+    "url": "/static/js/runtime-main.d499b9e3.js"
+  }
+]);
